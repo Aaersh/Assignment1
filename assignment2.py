e1f243e4e861ed889a968bb5af55b20e33f8086a
Aaersh <92209990+Aaersh@users.noreply.github.com>
2022-01-19 13:29:35 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,105 @@
+#****************************************** QUESTION 1 ***********************************************
+print("*************************************Question 1*************************************")
+inputstring = "Python is a case sensitive language."
+print(len(inputstring))                                             #to Find the length of the input string.
+print(inputstring[::-1])                                            #to Reverse the order of the string
+newstring = inputstring[10:26]                                      #to store “a case sensitive” in new string.
+print(newstring)
+print(inputstring.replace("a case sensitive", "object oriented"))   #to Replace “a case sensitive” with “object oriented”.
+print(inputstring.index("a"))                                       #to Find index of substring “a” in the given input string.
+print(inputstring.replace(" ", ""))                                 #to Remove the white spaces from the given input string.
+
+
+
+
+
+
+
+
+
+#****************************************** QUESTION 2 ***********************************************
+print("*************************************Question 2*************************************")
+name = "Aaersh Kumar"
+sid = "21103037"
+department = "Computer Science and Engineering"
+cgpa = "9.9"
+print("Hey,",name, "Here!\nMy SID is", sid, "\nI am from",department, "department and my CGPA is",cgpa)
+
+
+
+
+
+
+
+
+#****************************************** QUESTION 3 *********************************************
+print("*************************************Question 3*************************************")
+a = 56
+b = 10
+print(a&b)
+print(a|b)
+print(a^b)
+print(a<<2, b<<2)
+print(a>>2, b>>4)
+
+
+
+
+
+
+
+
+#****************************************** QUESTION 4 *********************************************
+#this is a program to find the greatest of three numbers entered by the user.
+print("*************************************Question 4*************************************")
+num1 = int(input("Enter first number\n"))
+num2 = int(input("Enter second number\n"))
+num3 = int(input("Enter third number\n"))
+
+if num1>num2:
+    if num1>num3:
+        print("the greatest of three numbers is", num1)
+    else:
+        print("the greatest of three numbers is", num3)
+
+else:
+    if num2>num3:
+        print("the greatest of three numbers is", num2)
+    else:
+        print("the greatest of three numbers is", num3)
+
+
+
+
+
+
+
+
+#****************************************** QUESTION 5 ***************************************
+print("*************************************Question 5*************************************")
+user_string = str(input("Enter the string\n"))
+index = user_string.find(" name ")
+if index==-1:
+    print("No, the word “name” is not present in the string.")
+else:
+    print("Yes, the word “name” is present in the string.")
+
+
+
+
+
+
+
+
+#****************************************** QUESTION 6 *********************************************
+#this is a program to check whether the given input lengths can form a triangle or not.
+print("*************************************Question 6*************************************")
+a = int(input("Enter length of first side\n"))
+b = int(input("Enter length of second side\n"))
+c = int(input("Enter length of third side\n"))
+
+if a+b>=c and b+c>=a and a+c>=b:                       #If any of the three lengths is greater than the sum of the other two, then you cannot form a triangle.
+    print("Yes, triangle can be formed.")
+
+else:
+    print("No, triangle cannot be formed.")
