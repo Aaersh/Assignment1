e4a052b80f4fe751dd1bfa493b8ef6a81e248c01
Aaersh <92209990+Aaersh@users.noreply.github.com>
2022-01-19 13:30:28 +0530

    Create assignment2_output.py

---
+++ assignment2_output.py
@@ -0,0 +1,37 @@
+*************************************Question 1*************************************
+36
+.egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language.
+10
+Pythonisacasesensitivelanguage.
+*************************************Question 2*************************************
+Hey, Aaersh Kumar Here!
+My SID is 21103037 
+I am from Computer Science and Engineering department and my CGPA is 9.9
+*************************************Question 3*************************************
+8
+58
+50
+224 40
+14 0
+*************************************Question 4*************************************
+Enter first number
+23
+Enter second number
+12
+Enter third number
+45
+the greatest of three numbers is 45
+*************************************Question 5*************************************
+Enter the string
+hello bois my name is aaersh
+Yes, the word “name” is present in the string.
+*************************************Question 6*************************************
+Enter length of first side
+8
+Enter length of second side
+6
+Enter length of third side
+4
+Yes, triangle can be formed.
