362c68aef5b020424f532259cd22949eb601dc5c
Aaersh <92209990+Aaersh@users.noreply.github.com>
2022-02-11 22:32:42 +0530

    Add files via upload

---
+++ assignment3_output.py
@@ -0,0 +1,151 @@
+******************************************************************* OUTPUT 1 **************************************************************
+#when a line is entered in the string:
+Enter a string
+it was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief
+The frequency of it is 5
+The frequency of was is 5
+The frequency of the is 5
+The frequency of best is 1
+The frequency of of is 5
+The frequency of times, is 2
+The frequency of worst is 1
+The frequency of age is 2
+The frequency of wisdom, is 1
+The frequency of foolishness, is 1
+The frequency of epoch is 1
+The frequency of belief is 1
+
+#when a word is entered in the string:
+Enter a string
+baggage
+The count of all characters in given string is :
+  {'g': 3, 'b': 1, 'a': 2, 'e': 1}
+
+
+
+
+
+
+
+
+******************************************************************* OUTPUT 2 **************************************************************
+Enter the date (1-31)
+31
+Enter the month (1-12)
+12
+Enter the year (1800-2025)
+2020
+the input date is 31 / 12 / 2020
+the next date is 1 / 1 / 2021
+
+
+
+
+
+
+
+
+
+******************************************************************* OUTPUT 3 **************************************************************
+Type 'y' to enter number / Type 'n' to cancel
+y
+Enter the number
+7
+Type 'y' to enter number / Type 'n' to cancel
+y
+Enter the number
+6
+Type 'y' to enter number / Type 'n' to cancel
+y
+Enter the number
+8
+Type 'y' to enter number / Type 'n' to cancel
+n
+Numbers you entered are : [7, 6, 8]
+Given number, square of that number : [(7, 49), (6, 36), (8, 64)]
+
+
+
+
+
+
+
+
+******************************************************************* OUTPUT 4 **************************************************************
+Please enter the grade (4-10)
+10
+Your Grade is ‘A+’ and Outstanding Performance.
+
+
+
+
+
+
+
+******************************************************************* OUTPUT 5 **************************************************************
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+
+
+
+
+******************************************************************* OUTPUT 6 **************************************************************
+Type 'y' to enter Name and SID / Type 'n' to cancel
+y
+Enter the SID
+21103037
+Enter the Name
+Aaersh
+Type 'y' to enter Name and SID / Type 'n' to cancel
+y
+Enter the SID
+21107012
+Enter the Name
+Manish
+Type 'y' to enter Name and SID / Type 'n' to cancel
+y
+Enter the SID
+21105116
+Enter the Name
+Vipul
+Type 'y' to enter Name and SID / Type 'n' to cancel
+n
+{'21103037': 'Aaersh', '21107012': 'Manish', '21105116': 'Vipul'}
+Dictionary after sorting it by key is : {'21103037': 'Aaersh', '21105116': 'Vipul', '21107012': 'Manish'}
+Dictionary after sorting it by value is : {'21103037': 'Aaersh', '21107012': 'Manish', '21105116': 'Vipul'}
+Enter the student's SID which you want to find
+21103037
+Name of the student whose SID is 21103037 is Aaersh
+
+
+
+
+
+
+
+
+******************************************************************* OUTPUT 7 **************************************************************
+Enter the number of terms you want to print in the fibonacci sequence
+10
+Fibonacci sequence:
+[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+The average of fibonacci sequence upto 10  terms is 8.8
+
+
+
+
+
+
+
+******************************************************************* OUTPUT 8 **************************************************************
+Set of all elements that are in Set1 and Set2 but not in both is {1, 3, 5, 6, 8}
+Set of all elements that are in only one of the three sets Set1, Set2 and Set3 is {3, 6, 8, 9, 13, 17}
+Set of elements that are exactly two of the sets Set1, Set2 and Set3 is {1, 2, 4, 5}
+Set of all integers in the range 1 to 10 that are not in Set1 is {6, 7, 8, 9, 10}
+set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3 is {10, 7}
